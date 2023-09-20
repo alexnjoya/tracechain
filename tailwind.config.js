8a66cca5f1e5d1008c2ee9f54b6859c694dfdf0e
alexnjoya <njoyaalexander71@gmail.com>
2023-09-20 00:54:50 +0000

    commit config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === "production", // Enable purge only in production
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
 };
